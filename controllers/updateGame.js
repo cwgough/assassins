b4b825d8a50c9746826917af328ae153898db0d3
Chris Gough <cwgough@bu.edu>
2023-02-14 21:50:54 -0500

    added killfeed

--- controllers/updateGame.js
+++ controllers/updateGame.js
@@ -1,9 +1,18 @@
 const userSchema = require("../models/userSchema")
+const axios = require('axios')
 
 async function killPlayer(req, res) {
   const killedPlayer = await userSchema.findOneAndUpdate({ name: req.body.name }, { alive: false })
   const newTarget = await userSchema.findOneAndUpdate({ name: killedPlayer.target }, { assassin: killedPlayer.assassin })
   const newAssassin = await userSchema.findOneAndUpdate({ name: killedPlayer.assassin }, { target: killedPlayer.target })
+
+  const slackText = `${killedPlayer.name} ${String.fromCodePoint(0x1F52B)} ${killedPlayer.assassin}`
+
+  axios.post(
+    'https://hooks.slack.com/services/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',
+    { "text": slackText }
+  )
+
   if (newAssassin.name == newTarget.name) {
     await userSchema.updateOne({ name: newAssassin.name }, { winner: true })
       .then(
